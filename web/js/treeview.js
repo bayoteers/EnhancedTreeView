58462b1cdd8d5bde0ca3166993d95736ee89fc9a
Eero Heino <eeheino@luba.research.nokia.com>
2011-04-14 07:23:45 +0000

    using ul instead of ol. some house cleaning. simple dependency loop detection

--- web/js/treeview.js
+++ web/js/treeview.js
@@ -1,33 +1,13 @@
-function doToggle_with_ol(node, event) {
-    var deep = event.altKey || event.ctrlKey;
-
-    if (node.nodeType == Node.TEXT_NODE)
-        node = node.parentNode;
-
-    var toggle = node.nextSibling;
-    while (toggle && toggle.tagName != "OL")
-        toggle = toggle.nextSibling;
-
-    if (toggle) {
-        if (deep) {
-            var direction = toggleDisplay(toggle, $(node).children('a').get(0));
-            changeChildren(toggle, direction);
-        } else {
-            toggleDisplay(toggle, $(node).children('a').get(0));
-        }
-    }
-    /* avoid problems with default actions on links (mozilla's
-     * ctrl/shift-click defaults, for instance */
-    event.preventBubble();
-    event.preventDefault();
-    return false;
+function expclo(node)
+{
+    $(node).parent().next('ul').toggle();
+    $(node).toggleClass('b_open').toggleClass('b_closed'); 
 }
 
 // save
 $('#save_tree').click(function(e)
 {
-    arraied = $('ol.sortable').nestedSortable('toArray', {startDepthCount: 0});
-    //alert('puuhp');
+    arraied = $('ul.sortable').nestedSortable('toArray', {startDepthCount: 0});
 
     $.post('page.cgi?id=treeview_ajax.html',
         {
@@ -35,13 +15,5 @@ $('#save_tree').click(function(e)
         },
         function () { alert('saved'); },
         'json');
-
-//        schema: schema,
-//        action: 'set',
-//        obj_id: obj_id,
-//        data: JSON.stringify(data_lists)
-//    arraied = dump(arraied);
-//    (typeof($('#toArrayOutput')[0].textContent) != 'undefined') ?
-//    $('#toArrayOutput')[0].textContent = arraied : $('#toArrayOutput')[0].innerText = arraied;
 });
 
