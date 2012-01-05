04c9adcf6f8aab6521dc947abe3cc11d6db547a6
David Wilson <dw@botanicus.net>
2012-01-05 18:05:19 +0200

    Refactor JavaScript into a class-like object with no globals.

--- web/js/DependencyEditor.js
+++ web/js/DependencyEditor.js
@@ -17,109 +17,154 @@
   
    Contributor(s):
      Visa Korhonen <visa.korhonen@symbio.com>
+     David Wilson <dw@botanicus.net>
   */
 
-var description_editable = false;
-var cancelButton = false;
-var saveButton = false;
-var originalSelectedIndex = 0;
+var DependencyEditor = {
+    /**
+     * Initialize the object and install our (bubbling) onblur handler.
+     */
+    init: function()
+    {
+        this.state = null;
+        $(document).focusout(this._onFocusOut.bind(this));
+    },
 
-function makeDependencyInfoEditable(blocked_bugid, dependson_bugid, desc_value, type_value) {
-    if(description_editable) {
-        makeDescriptionStatic();
-        description_editable = false;
-        return;
-    }
-    var elem = $('#dep_' + blocked_bugid + '_' + dependson_bugid);
-    var par = elem[0].parentNode;
-    $(par).after(getEditableDependencyInfoHtml(blocked_bugid, dependson_bugid, desc_value));
-    var sel_elem = $("#deptypesel");
-    originalSelectedIndex = type_value;
-    sel_elem[0].selectedIndex = type_value;
-    $("#description").focus();
-    description_editable = true;
-}
-
-$(document).ready(putHandler);
-
-function putHandler() {
-    $("*").focus(indicateFocus);
-}
-
-function indicateFocus(event) {
-    var x=event.target; 
-    if(x.id != "description" && x.id != "deptypesel") {
-        makeDescriptionStatic();
-    }
-}
-
-function getEditableDependencyInfoHtml(blocked_bugid, dependson_bugid, desc_value)
-{
-    var template = $('#desc_row_template').clone();
-    $('#description', template).val(desc_value);
-    $('#blocked_bugid', template).val(blocked_bugid);
-    $('#dependson_bugid', template).val(dependson_bugid);
-    return template;
-}
-
-function makeDescriptionStatic() {
-    var elem = $("#description");
-    var text = elem[0].value;
-    var original = elem[0].defaultValue;
-
-    var sel = $("#deptypesel");
-    var selectedIndex = sel[0].selectedIndex;
-
-    if(text != original || selectedIndex != originalSelectedIndex) {
-        if(saveButton) {
-            saveDepDescription(text, selectedIndex);
+    /**
+     * Respond to any element losing focus by 
+     */
+    _onFocusOut: function(event)
+    {
+        var sensitive = { description: 1, deptypesel: 1 };
+        if(! (sensitive[event.target.id] && this.state)) {
+            return;
         }
-        else if(!cancelButton) {
-            if(confirm("Do you want to save description of dependence?")) {
-                saveDepDescription(text, selectedIndex);
-            }
+
+        var confirmMsg = "Do you want to save description of dependence?";
+        if(this.isDirty() && confirm(confirmMsg)) {
+            this.save();
+        } else {
+            this.hide();
+        }
+    },
+
+    /**
+     * Render the editor and insert it into the DOM.
+     */
+    _render: function()
+    {
+        var state = this.state;
+        var template = $('#desc_row_template').clone();
+
+        $('#deptypesel', template).val(state.dep_type);
+        $('#description', template).val(state.description);
+        $('#blocked_bugid', template).val(state.blocked);
+        $('#dependson_bugid', template).val(state.dependson);
+        $('span[title=cancel]', template).click(this.hide.bind(this));
+        $('span[title=save]', template).click(this.save.bind(this));
+        state.template = template;
+
+        var elem = $('#dep_' + state.blocked + '_' + state.dependson);
+        elem.parent().after(template);
+        $("#description", template).focus();
+    },
+
+    /**
+     * @param blocked
+     *      Integer ID of the blocked bug in this relation ("parent bug").
+     * @param dependson
+     *      Integer ID of the depended-on bug in this relation ("child bug").
+     * @param description
+     *      String dependency description.
+     * @param dep_type
+     *      Integer dependency type (see deptypesel in
+     *      EnhancedTreeView.html.tmpl)..
+     */
+    show: function(blocked, dependson, description, dep_type)
+    {
+        this.hide();
+
+        this.state = {
+            blocked: blocked, // renamed: blocked_bugid
+            dependson: dependson, // renamed: dependson_bugid
+            description: description,
+            dep_type: dep_type // renamed: type_value
+        };
+
+        // Stash a string copy of state away later to compare with form to
+        // see if it changed.
+        this.state.origParams = $.param(this.state);
+
+        this._render();
+    },
+
+    /**
+     * Hide the editor if visible, and discard any state.
+     */
+    hide: function()
+    {
+        if(this.state) {
+            this.state.template.remove();
+            this.state = null;
+        }
+    },
+
+    /**
+     * Return an object containing parameters describing the dependency.
+     */
+    getParams: function()
+    {
+        return {
+            blocked: this.state.blocked,
+            dependson: this.state.dependson,
+            description: $('#description', this.state.template).val(),
+            deptype: $('#deptypesel', this.state.template).val()
+        };
+    },
+
+    /**
+     * Return true if editor is currently visible and the form has been
+     * modified by the user.
+     */
+    isDirty: function()
+    {
+        var state = this.state;
+        return state && $.param(this.getParams()) != state.origParams;
+    },
+
+    /**
+     * Saves dependency description into database using AJAX call.
+     */
+    save: function()
+    {
+        if(! this.isDirty()) {
+            return;
+        }
+
+        $.post('page.cgi?id=EnhancedTreeView_ajax.html', {
+            schema: 'depinfo',
+            action: 'update',
+            data: JSON.stringify({
+                id: 0,
+                method: "Depinfo.update",
+                params: this.getParams()
+            })
+        }, this._onSaveDone.bind(this), 'text');
+    },
+
+    /**
+     * Callback invoked after succesfull AJAX call if succesfull, if server
+     * responds without throwing exception. Ordered errors are shown in error
+     * message. Function shows status of saving to user.
+     */
+    _onSaveDone: function(response, status, xhr)
+    {
+        var retObj = $.parseJSON(response);
+        if (retObj.errors) {
+            alert("There are errors: " + retObj.errormsg);
+        } else {
+            alert("Success");
+            window.location.reload();
         }
     }
-    elem = $("#desc_row").remove();
-
-    saveButton = false;
-    cancelButton = false;
-    description_editable = false;
-}
-
-
-/**
- * Function saves value of dependency description into database by doing ajax-call.
- */
-function saveDepDescription(newValue, newSelection) {
-    $.post('page.cgi?id=EnhancedTreeView_ajax.html', {
-        schema: 'depinfo',
-        action: 'update',
-        data: JSON.stringify({
-            id: 0,
-            method: "Depinfo.update",
-            params: {
-                blocked: $('#blocked_bugid').val(),
-                dependson: $('#dependson_bugid').val(),
-                description: newValue,
-                deptype: newSelection
-            }
-        })
-    }, saveDepResponse, 'text');
-}
-
-
-/**
- * Function is call-back function, that is called after succesfull ajax call returns.
- * Ajax call if succesfull, if server responds without throwing exception. Ordered
- * errors are shown in error message. Function shows status of saving to user.
- */
-function saveDepResponse(response, status, xhr) {
-    var retObj = $.parseJson(response);
-    if (retObj.errors) {
-        alert("There are errors: " + retObj.errormsg);
-    } else {
-        alert("Success");
-        window.location.reload();
-    }
-}
+};
